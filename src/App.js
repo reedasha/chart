40b8a5c85ba2843ba1e32c21cfd975d62d6f0053
Ree Daria <ri_d@auca.kg>
2018-09-28 23:16:42 +0600

    Fix

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <MultipleChartComponent/>
-
-          <LineChartComponent/>
+        <PieChartComponent/>
+        <LineChartComponent/>
+        <MultipleChartComponent/>
       </div>
     );
   }
